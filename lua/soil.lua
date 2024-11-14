e8c7a50d22dec7f2afc023f2cdae0eabf8ddaa2c
John Naeder <johnnaeder@gmail.com>
2024-11-14 19:52:40 +0700

    Alter jpegview for window

--- lua/soil.lua
+++ lua/soil.lua
@@ -1,45 +1,49 @@
-local Logger = require('soil.logger'):new("Soil")
+local Logger = require("soil.logger"):new("Soil")
 local M = {}
 
 M.DEFAULTS = {
-    image = {
-        darkmode = false,
-        format = "png",
-        execute_to_open = function(img) return "nsxiv -b " .. img end
-    }
+  image = {
+    darkmode = false,
+    format = "png",
+    execute_to_open = function(img)
+      local viewer = os.getenv("OS") == "Windows_NT" and "jpeview" or "nsxiv -b"
+      return viewer .. " " .. img
+    end,
+  },
 }
 
 function M.setup(opts)
-    if opts.puml_jar then
-        M.DEFAULTS.puml_jar = opts.puml_jar
-    end
+  if opts.puml_jar then
+    M.DEFAULTS.puml_jar = opts.puml_jar
+  end
 
-    local img = opts.image or {}
+  local img = opts.image or {}
 
-    if img.format then
-        if img.format == "png" or img.format == "svg" then
-            M.DEFAULTS.image.format = img.format
-        else
-            Logger:error("Setup Error: The values allowed for image.format are 'png' or 'svg'.")
-        end
+  if img.format then
+    if img.format == "png" or img.format == "svg" then
+      M.DEFAULTS.image.format = img.format
+    else
+      Logger:error(
+        "Setup Error: The values allowed for image.format are 'png' or 'svg'."
+      )
     end
+  end
 
-    if img.darkmode ~= nil then
-        if type(img.darkmode) == "boolean" then
-            M.DEFAULTS.image.darkmode = img.darkmode
-        else
-            Logger:error("Setup Error: image.darkmode must be a boolean value.")
-        end
+  if img.darkmode ~= nil then
+    if type(img.darkmode) == "boolean" then
+      M.DEFAULTS.image.darkmode = img.darkmode
+    else
+      Logger:error("Setup Error: image.darkmode must be a boolean value.")
     end
+  end
 
-    if img.execute_to_open then
-        if type(img.execute_to_open) == "function" then
-            M.DEFAULTS.image.execute_to_open = img.execute_to_open
-        else
-            Logger:error("Setup Error: image.execute_to_open must be a function.")
-        end
+  if img.execute_to_open then
+    if type(img.execute_to_open) == "function" then
+      M.DEFAULTS.image.execute_to_open = img.execute_to_open
+    else
+      Logger:error("Setup Error: image.execute_to_open must be a function.")
     end
+  end
 end
 
 return M
-
